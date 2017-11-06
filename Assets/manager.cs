bb1f0cf362936738e54e5d2e161e6720b5faf59e
madebyskippy <timothys@nyu.edu>
2017-11-06 12:55:58 -0500

    worx with 10 keys as intended

--- Assets/manager.cs
+++ Assets/manager.cs
@@ -9,55 +9,87 @@ public class manager : MonoBehaviour {
 	[SerializeField] Text seqtext;
 
 	GameObject[] frames;
-	bool[] frameState;
 	int numActive;
-	List<int> frameActive = new List<int>();
+	List<int> frameInput = new List<int>();
 
-	char[] keys = new char[10]{'Q','W','E','R','T','Y','U','I','O','P'};
+	char[] keys = new char[10]{'Q','W','E','R','T','Y','U','I','O','P'}; //just used for getting input
 
-	int[] sequence = new int[10]{0,1,2,3,4,5,6,7,8,9};
+	int[] sequence = new int[10]{0,1,2,3,4,5,6,7,8,9}; //used for getting keypress > frame shown
+	string seqstring = ""; //the actual answer to the sequence
 
 	// Use this for initialization
 	void Start () {
 		seqtext.text = "";
-		numActive = 0;
+		numActive = 3;
 		Object[] s = Resources.LoadAll ("getup", typeof(Sprite));
 		frames = new GameObject[s.Length];
-		frameState = new bool[s.Length];
 		for (int i = 0; i < s.Length; i++) {
 			frames [i] = new GameObject ();
 			SpriteRenderer sr = frames [i].AddComponent<SpriteRenderer> ();
 			sr.sprite = s [i] as Sprite;
-			frameState [i] = false;
-			frames [i].SetActive (frameState[i]);
+			frameInput.Add (i);
 		}
+		randomizeSequence ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		for (int i = 0; i < keys.Length; i++) {
 			if (Input.GetKeyDown(KeyCode.A+((int)keys[i])-65)){
-				if (!frameState[i]) {
-					frameState [i] = true;
-					numActive++;
-					frameActive.Add (i);
-					seqtext.text += keys [i];
-					if (numActive > 3) {
-						frameState [frameActive [0]] = false;
-						frameActive.RemoveAt (0);
-						numActive--;
-						seqtext.text = seqtext.text.Remove (0, 1);
-					}
-				}
+				frameInput.Remove (i);
+				frameInput.Insert (0, i);
 			}
 		}
 
-		for (int j = 0; j < frames.Length; j++) {
-			frames [j].SetActive (frameState[j]);
+		seqtext.text = ". . . ";
+		string inp = "";
+		for (int j = 0; j < frameInput.Count; j++) {
+			if (j < numActive) {
+				frames [sequence[frameInput[j]]].SetActive (true);
+				seqtext.text = " "+ keys[frameInput[j]] + seqtext.text;
+			} else {
+				frames [sequence[frameInput[j]]].SetActive (false);
+			}
+			inp += keys[frameInput [j]];
+		}
+//		Debug.Log (inp);
+		seqtext.text = ". . ." + seqtext.text;
+
+		if (check ()) {
+			Debug.Log ("win!!!");
+			seqtext.text = "win!!!!!!!!!!";
 		}
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			SceneManager.LoadScene ("prototype");
 		}
+	}
+
+	void randomizeSequence(){
+		string s = "0000000000";
+		string s2="";
+		List<int> seq = new List<int> (sequence);
+		for (int i = 0; i < sequence.Length; i++) {
+			int x = seq [Random.Range (0, seq.Count)];
+			seq.Remove (x);
+			sequence [i] = x;
+			s = s.Insert (x, keys[i].ToString());
+			s = s.Remove (x+1,1);
+			s2 += keys[x].ToString ();
+		}
+		seqstring = s;
+		Debug.Log (s);
+	}
+
+	bool check(){
+
+		for (int i = 0; i < sequence.Length; i++) {
+			if (keys[frameInput [frameInput.Count-1-i]].ToString() != seqstring[i].ToString()) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
